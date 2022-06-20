6a017c4c77527e001551e585028700dd4f8f1f7d
manthinh <kbkl0224@i.softbank.jp>
2022-06-21 00:09:11 +0900

    インデント整理

--- resources/views/Form.blade.php
+++ resources/views/Form.blade.php
@@ -13,40 +13,22 @@
     <div>
         <table>
             <tr>
-                <td>
-                    <h2>
-                        名前：
-                    </h2>
-                </td>
-                <td>
-                    <input type="text" name="name" placeholder="名前">
-                </td>
+                <td><h2>名前：</h2></td>
+                <td><input type="text" name="name" placeholder="名前"></td>
             </tr>
 
             <tr>
-                <td>
-                    <h2>
-                        Eメール：
-                    </h2>
-                </td>
-                <td>
-                    <input type="text" name="email" placeholder="eメール">
-                </td>
+                <td><h2>Eメール：</h2></td>
+                <td><input type="text" name="email" placeholder="eメール"></td>
             </tr>
 
             <tr>
-                <td>
-                    <h2>
-                        住所：
-                    </h2>
-                </td>
-                <td>
-                    <input type="text" name="address" placeholder="住所">
-                </td>
+                <td><h2>住所：</h2></td>
+                <td><input type="text" name="address" placeholder="住所"></td>
 
-                <tr>
+            <tr>
                     <input type="submit" value="登録">
-                </tr>
+            </tr>
         </table>
     </div>
     </form>
