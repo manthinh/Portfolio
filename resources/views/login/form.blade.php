f7513cfdcfe35ab9599fe20d1166b53a5564150c
manthinh <kbkl0224@i.softbank.jp>
2022-06-24 14:30:25 +0900

    view復習

--- resources/views/login/form.blade.php
+++ resources/views/login/form.blade.php
@@ -1,58 +1,55 @@
-<!DOCTYPE html>
-<head>
-    <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>新規登録画面</title>
-</head>
-<body>
-    <form action="{{ route('signUp')}}" method="post">
-        @csrf
+@extends('layouts.loginIndex')
+
+@section('title','新規登録フォーム')
+    
+<form action="{{ route('signUp')}}" method="post">
+    @csrf
     
     <h1>新規登録フォーム</h1>
-
+    
     @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
     @endif
-
-
+    
+    
     <div>
         <table>
             <tr>
                 <td><h2>名前：</h2></td>
                 <td><input type="text" name="name" placeholder="名前"></td>
             </tr>
-
+            
             <tr>
                 <td><h2>Eメール：</h2></td>
                 <td><input type="email" name="email" placeholder="eメール"></td>
             </tr>
-
+            
             <tr>
                 <td><h2>住所：</h2></td>
                 <td><input type="text" name="address" placeholder="住所"></td>
-
-            <tr>
-
-            <tr>
-                <td><h2>パスワード：</h2></td>
-                <td><input type="password" name="password" placeholder="パスワード"></td>
-            </tr>
-            <tr>
-                <td><h2>パスワード確認用：</h2></td>
-                <td><input type="password" name="comfirm-password" placeholder="パスワード確認用"></td>
-            </tr>
-
-            <tr>
-                    <input type="submit" value="登録">
-            </tr>
-            
-        </table>
-    </div>
-    </form>
-</body>
+                
+                <tr>
+                    
+                    <tr>
+                        <td><h2>パスワード：</h2></td>
+                        <td><input type="password" name="password" placeholder="パスワード"></td>
+                    </tr>
+                    <tr>
+                        <td><h2>パスワード確認用：</h2></td>
+                        <td><input type="password" name="comfirm-password" placeholder="パスワード確認用"></td>
+                    </tr>
+                    
+                    <tr>
+                        <input type="submit" value="登録">
+                    </tr>
+                    
+                </table>
+            </div>
+        </form>
+    </body>
