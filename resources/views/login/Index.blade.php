110ed993fc89cacfca364929ac6160a955663536
manthinh <kbkl0224@i.softbank.jp>
2022-07-20 01:22:53 +0900

    プルリク修正

--- resources/views/login/Index.blade.php
+++ resources/views/login/Index.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.app')
 @section('body')
-@section('title','ログイン画面')
+    @section('title','ログイン画面')
 
-<p>{{ session('flash_massage')}}</p>
-<form action="{{ route('login.signIn')}}" method="post">
-    @csrf
+    <p>{{ session('flash_massage')}}</p>
+    <form action="{{ route('login.signIn')}}" method="post">
+        @csrf
 
-    <h1>ログイン画面</h1>        
-    <a href="{{ route('sign-up.form')}}">新規登録はこちら</a>
-    <a href="{{ route('admin.loginIndex')}}">developer only</a>
+        <h1>ログイン画面</h1>
+        <p>
+            <a href="{{ route('sign-up.form')}}">新規登録はこちら</a>
+            <a href="{{ route('admin.loginIndex')}}">developer only</a>
+        </p>
 
-    <p>登録済みのEメール、パスワードをご入力の上「ログイン」ボタンを押してください。</p>
+        <p>登録済みのEメール、パスワードをご入力の上「ログイン」ボタンを押してください。</p>
 
-    <input type="id" name="email" placeholder="Eメール" required>
-    
+        <input type="email" name="email" placeholder="Eメール" required>
 
-    <input type="password" name="password" placeholder="パスワード" required>
-    
-    <input type="submit" value="ログイン"　class = button>
-</form>
+
+        <input type="password" name="password" placeholder="パスワード" required>
+
+        <input type="submit" value="ログイン">
+    </form>
 @endsection
