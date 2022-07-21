a16c774df38fb9dcd78073e7d8f9cc980e745226
manthinh <kbkl0224@i.softbank.jp>
2022-07-22 00:30:31 +0900

    プルリク修正

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,46 +13,52 @@ use Illuminate\Support\Facades\Hash;
 class LoginController extends Controller
 {
 
-    public function loginIndex() {
+    public function loginIndex()
+    {
         return view('login.Index');
     }
 
-    public function signIn(Request $request) {
-        $email =  $request->input('email');
-        $password =  $request->input('password');
-        $userInfo = User::where('email',$email)->first();
-        if ($userInfo && Hash::check($password,$userInfo->password)) {
-            $request->session()->put('doneLogin',$userInfo->id);
+    public function signIn(Request $request)
+    {
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $userInfo = User::where('email', $email)->first();
+        if ($userInfo && Hash::check($password, $userInfo->password)) {
+            $request->session()->put('doneLogin', $userInfo->id);
             return redirect()->route('myPage.myPage');
         }
-        session()->flash('flash_massage','登録済みのEメールとパスワードが一致しません。');
+        session()->flash('flash_massage', '登録済みのEメールとパスワードが一致しません。');
         return redirect()->route('loginIndex');
-        
-    }
-    
-    public function myPage() {
-    return view('myPage');
+
     }
 
-    public function form() {
+    public function myPage()
+    {
+        return view('myPage');
+    }
+
+    public function form()
+    {
         return view('login.form');
     }
-    
-    public function signUp(LoginUserCreateRequest $request) {
+
+    public function signUp(LoginUserCreateRequest $request)
+    {
         $validated = $request->validated();
-        $request->session()->put('usersInfo',$validated);
+        $request->session()->put('usersInfo', $validated);
         User::create([
             'name' => $validated['name'],
             'email' => $validated['email'],
             'address' => $validated['address'],
             'password' => Hash::make($validated['password']),
-            ]);
-            return redirect()->route('sign-up.confirm');
-        }
-        
-        public function confirm(Request $request) {
-            $value = $request->session()->get('usersInfo');
-            return view('login.confirm',compact('value'));
-        }
+        ]);
+        return redirect()->route('sign-up.confirm');
+    }
+
+    public function confirm(Request $request)
+    {
+        $value = $request->session()->get('usersInfo');
+        return view('login.confirm', compact('value'));
+    }
 
 }
